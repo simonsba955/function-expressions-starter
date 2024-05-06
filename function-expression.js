6dfad9c934155561ff9dd46a3f83e669825bd63c
bengal865 <69059209+bengal865@users.noreply.github.com>
2024-05-06 07:55:41 -0400

    Update function-expression.js

--- function-expression.js
+++ function-expression.js
@@ -3,21 +3,21 @@
 // JS Function Expression Example
 
 // Assign function add to a variable named sum
-const sum = function(num1, num2){
+const calcSum = function(num1, num2){
     return num1 + num2;
 }
 
 // Find the sum by calling the function
 // Result of calculation is returned from the function and
 // stored in a new variable named answer
-const answer = sum(12, 5);
+const answer = calcSum(12, 5);
 // Option 1
 // Displaying the result of the calculation in the browser console
 console.log(`(Option 1): The sum is: ${answer}`);
 // Option 2
-// Calling the function sum from within the template string
-console.log(`(Option 2): The sum is: ${sum(20, 4)}`);
+// Calling the function calcSum from within the template string
+console.log(`(Option 2): The sum is: ${calcSum(20, 4)}`);
 // Calling the function sum from within the template string
 const firstNum = 20;
 const secondNum = 4;
-console.log(`(Option 3): The sum is: ${sum(firstNum, secondNum)}`);
+console.log(`(Option 3): The sum is: ${calcSum(firstNum, secondNum)}`);
